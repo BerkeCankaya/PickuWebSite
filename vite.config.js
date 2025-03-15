8b1b447c6df3812ee9180661eb8c903bcad5b9a7
Berke Cankaya <berke35.5@hotmail.com>
2025-03-15 23:35:12 +0300

    404 error

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs/promises';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: '/PickuWebSite/', 
+  base: "/PickuWebSite/",
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 })
