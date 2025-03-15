ecafbcd90f5d8165510434c1459ea0e1f05d07db
Berke Cankaya <berke35.5@hotmail.com>
2025-03-15 23:32:39 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs/promises';
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PickuWebSite/",
+  base: '/PickuWebSite/', 
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
